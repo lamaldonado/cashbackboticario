8ed95abb72b42a86edb3916a726da2b8771b8827
Luiz Antonio Maldonado Marthos <luiz.marthos@bematech.com.br>
2020-02-28 15:53:56 -0300

    Setting up environment

---
+++ src/index.js
@@ -0,0 +1,1 @@
+module.exports.helloword = () => 'Hello Word';
---
+++ test/test.spec.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const { expect } = chai;
+
+describe('1 === 1', () => {
+  it('1 === 1', () => {
+    expect(1).to.be.eq(1);
+  });
+});
